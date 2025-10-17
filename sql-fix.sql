4918279bd1ac5f02b34d5b8526013cf7b3137c17
kovarex <kovarex@gmail.com>
2025-10-17 15:57:46 +0200

    Another fix.

--- sql-fix.sql
+++ sql-fix.sql
@@ -1,3 +1,4 @@
 -- set of fixes to do on the tsumego hero database once it is moved to our servers
 
 ALTER TABLE `user_boards` DROP COLUMN `id`;
+ALTER TABLE ranks ALTER COLUMN points SET DEFAULT 0;
