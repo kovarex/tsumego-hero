20707eda2083819220fea4458ac9361a6df4e870
kovarex <kovarex@gmail.com>
2025-10-21 02:47:27 +0200

    Test (only works when EmptyIntegerBehaviorTest gets removed.

---
+++ tests/TestCase/Controller/Component/RatingTest.php
@@ -0,0 +1,26 @@
+<?php
+
+// App::uses('Component', 'Rating'); or anything similar doesn't work
+require_once("src/Controller/Component/RatingComponent.php");
+
+class RatingComponentTest extends ControllerTestCase
+{
+  public function testRatingToReadableRank(): void
+  {
+    $this->assertSame(Rating::getReadableRankFromRating(-100), '22k');
+    $this->assertSame(Rating::getReadableRankFromRating(0), '21k');
+    $this->assertSame(Rating::getReadableRankFromRating(100), '20k');
+    $this->assertSame(Rating::getReadableRankFromRating(200), '19k');
+    $this->assertSame(Rating::getReadableRankFromRating(2049), '1k');
+    $this->assertSame(Rating::getReadableRankFromRating(2050), '1d');
+    $this->assertSame(Rating::getReadableRankFromRating(2051), '1d');
+    $this->assertSame(Rating::getReadableRankFromRating(2150), '2d');
+    $this->assertSame(Rating::getReadableRankFromRating(2700), '7d');
+    $this->assertSame(Rating::getReadableRankFromRating(2749), '7d');
+    $this->assertSame(Rating::getReadableRankFromRating(2750), '8d');
+    $this->assertSame(Rating::getReadableRankFromRating(2779), '8d');
+    $this->assertSame(Rating::getReadableRankFromRating(2780), '9d');
+    $this->assertSame(Rating::getReadableRankFromRating(2809), '9d');
+    $this->assertSame(Rating::getReadableRankFromRating(2810), '10d');
+  }
+}
