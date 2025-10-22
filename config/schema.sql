008f0b44d0d99b880c42b27602b0f892ec34815d
kovarex <kovarex@gmail.com>
2025-10-22 13:14:10 +0200

    Fixed schema.sql, the id column is in the database we import, and must be in the schema, so we can use the same sql-fix on the schema as on the future database we will import and fix.

--- config/schema.sql
+++ config/schema.sql
@@ -688,6 +688,7 @@ CREATE TABLE `tsumegos` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_boards` (
+  `id` int NOT NULL,
   `user_id` int NOT NULL,
   `b1` int NOT NULL DEFAULT '1',
   `b2` int NOT NULL DEFAULT '1',
