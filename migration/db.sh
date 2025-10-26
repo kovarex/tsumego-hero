ddf0b5d1aab719cae14592b221079041e9b25a44
kovarex <kovarex@gmail.com>
2025-10-26 22:27:07 +0100

    Migration tweaks.

--- migration/db.sh
+++ migration/db.sh
@@ -3,5 +3,9 @@ sed -i 's/CHARSET=[a-z_0-9]*/CHARSET=utf8mb4/g' tsumego-hero-db-dump.sql
 sed -i 's/CHARACTER SET [a-z_0-9]*/CHARACTER SET utf8mb4/g' tsumego-hero-db-dump.sql
 sed -i 's/COLLATE=[a-z_0-9]*/COLLATE=utf8mb4_unicode_ci/g' tsumego-hero-db-dump.sql
 sed -i 's/COLLATE [a-z_0-9]*/COLLATE utf8mb4_unicode_ci/g' tsumego-hero-db-dump.sql
-mysql --host=tsumego.db -u kovarex -p db < tsumego-hero-db-dump.sql
-mysql --host=tsumego.db -u kovarex -p db < sql-fix.sql
+export host_parameter="--host=$1"
+
+if [ "localhost" = $1 ]; then
+  export host_parameter=
+fi
+mysql $host_parameter -u $2 -p $3 < tsumego-hero-db-dump.sql < sql-fix.sql
