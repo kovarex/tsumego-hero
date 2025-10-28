13b61b327c1f1ae95b8574307d480ff4429c7a62
kovarex <kovarex@gmail.com>
2025-10-28 22:21:46 +0100

    WIP experiment.

---
+++ selenium/setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux64.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+export PATH=$PATH:/var/www/html/selenium/.
