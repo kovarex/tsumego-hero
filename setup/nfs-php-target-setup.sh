89e04f01f1f8b72a42263512196849a5a69fbaf1
kovarex <kovarex@gmail.com>
2025-11-03 13:21:23 +0100

    File location update

--- setup/nfs-php-target-setup.sh
+++ setup/nfs-php-target-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/bin
-cp setup/php-redirect ~/bin/php
+cp setup/files/php-redirect ~/bin/php
 chmod +x ~/bin/php
-cp setup/nfs-permanent ~/.bashrc
-cp setup/nfs-permanent ~/.profile
+cp setup/files/nfs-permanent ~/.bashrc
+cp setup/files/nfs-permanent ~/.profile
