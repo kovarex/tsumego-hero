005f92a22bd01eb703aa5b699ddfebf88fb0e4a2
kovarex <kovarex@gmail.com>
2025-11-03 05:23:02 +0100

    Portable sed.

--- migration/run.sh
+++ migration/run.sh
@@ -4,13 +4,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo Working in $SCRIPT_DIR
 cd $SCRIPT_DIR
 
+rm db-dump.sql
 echo Downloading db file
 curl https://tsumego-hero.com/files/$1 > db-dump.sql
 echo fixing sql file
-sed -i 's/CHARSET=[a-z_0-9]*/CHARSET=utf8mb4/g' db-dump.sql
-sed -i 's/CHARACTER SET [a-z_0-9]*/CHARACTER SET utf8mb4/g' db-dump.sql
-sed -i 's/COLLATE=[a-z_0-9]*/COLLATE=utf8mb4_unicode_ci/g' db-dump.sql
-sed -i 's/COLLATE [a-z_0-9]*/COLLATE utf8mb4_unicode_ci/g' db-dump.sql
+sed 's/CHARSET=[a-z_0-9]*/CHARSET=utf8mb4/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+sed 's/CHARACTER SET [a-z_0-9]*/CHARACTER SET utf8mb4/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+sed 's/COLLATE=[a-z_0-9]*/COLLATE=utf8mb4_unicode_ci/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+sed 's/COLLATE [a-z_0-9]*/COLLATE utf8mb4_unicode_ci/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
 export host_parameter="--host=$2"
 
 if [ "localhost" = $2 ]; then
