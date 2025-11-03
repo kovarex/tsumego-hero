cf443c95afd9a7b53f116064f54f98a8657042f9
kovarex <kovarex@gmail.com>
2025-11-03 01:05:30 +0100

    Applied on test.tsumego.

--- migration/sql-fix.sql
+++ migration/sql-fix.sql
@@ -174,7 +174,6 @@ ALTER TABLE `sgf` CHANGE `id` `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /* was needed because of besogo superko bug, which is fixed now, so it is used everywhere. */
 ALTER TABLE tsumego DROP COLUMN virtual_children;
 
-/* test.tsumego version until this point */
 ALTER TABLE tsumego RENAME COLUMN elo_rating_mode to `rating`;
 ALTER TABLE user RENAME COLUMN elo_rating_mode to `rating`;
 
