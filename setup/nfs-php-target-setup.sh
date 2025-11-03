4bebb0a8bce732550252cc0a84ffa825e17a6408
kovarex <kovarex@gmail.com>
2025-11-03 13:22:15 +0100

    Filename fix

--- setup/nfs-php-target-setup.sh
+++ setup/nfs-php-target-setup.sh
@@ -3,5 +3,5 @@
 mkdir -p ~/bin
 cp setup/files/php-redirect ~/bin/php
 chmod +x ~/bin/php
-cp setup/files/nfs-permanent ~/.bashrc
-cp setup/files/nfs-permanent ~/.profile
+cp setup/files/nfs-permanent.sh ~/.bashrc
+cp setup/files/nfs-permanent.sh ~/.profile
