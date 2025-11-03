1d03df605587eeca1ed24d33d9e10bb7276f2394
kovarex <kovarex@gmail.com>
2025-11-03 05:41:29 +0100

    Also create the database

--- migration/run.sh
+++ migration/run.sh
@@ -7,16 +7,21 @@ cd $SCRIPT_DIR
 rm db-dump.sql
 echo Downloading db file
 curl https://tsumego-hero.com/files/$1 > db-dump.sql
-echo fixing sql file
+echo fixing sql file 1/4
 sed 's/CHARSET=[a-z_0-9]*/CHARSET=utf8mb4/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+echo fixing sql file 2/4
 sed 's/CHARACTER SET [a-z_0-9]*/CHARACTER SET utf8mb4/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+echo fixing sql file 3/4
 sed 's/COLLATE=[a-z_0-9]*/COLLATE=utf8mb4_unicode_ci/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
+echo fixing sql file 4/4
 sed 's/COLLATE [a-z_0-9]*/COLLATE utf8mb4_unicode_ci/g' db-dump.sql > tmp.sql && mv tmp.sql db-dump.sql
 export host_parameter="--host=$2"
 
 if [ "localhost" = $2 ]; then
   export host_parameter=
 fi
+echo creating database
+echo CREATE DATABASE $4 > mysql $host_parameter -u $3 -p < db-dump.sql
 echo importing database
 mysql $host_parameter -u $3 -p $4 < db-dump.sql
 /usr/local/bin/php8.4 /usr/local/bin/composer migrate
