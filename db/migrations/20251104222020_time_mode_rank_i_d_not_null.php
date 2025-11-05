481dd8a8cd7f3b737d88f69080fe9ebe192257c4
kovarex <kovarex@gmail.com>
2025-11-05 01:13:42 +0100

    There are apparently few entries where this id isn't matching, so we need to delete those before changint the column to be not nullable.

--- db/migrations/20251104222020_time_mode_rank_i_d_not_null.php
+++ db/migrations/20251104222020_time_mode_rank_i_d_not_null.php
@@ -7,6 +7,7 @@ use Phinx\Migration\AbstractMigration;
 final class TimeModeRankIDNotNull extends AbstractMigration
 {
 	public function change(): void {
+		$this->execute("DELETE FROM `time_mode_session` WHERE time_mode_rank_id IS NULL;");
 		$this->execute("ALTER TABLE `time_mode_session` CHANGE `time_mode_rank_id` `time_mode_rank_id` INT UNSIGNED NOT NULL;");
 	}
 }
