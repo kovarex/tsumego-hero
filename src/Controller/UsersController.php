4003e71bb67eb25486b42dba16ba060dfce9586a
kovarex <kovarex@gmail.com>
2025-11-25 22:51:32 +0100

    cs fix

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1760,7 +1760,7 @@ then ignore this email. https://' . $_SERVER['HTTP_HOST'] . '/users/newpassword/
 
 		$this->signIn($user);
 		$this->response->statusCode(303);
-		$this->response->header('Location',Router::url('/', true));
+		$this->response->header('Location', Router::url('/', true));
 		$this->response->send();
 		exit;
 	}
