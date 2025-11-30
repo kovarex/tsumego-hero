9074877e7e805534ffad58aa2e2479158c892958
kovarex <kovarex@gmail.com>
2025-11-30 04:21:39 +0100

    Cleanup

--- webroot/js/TagConnectionsEdit.js
+++ webroot/js/TagConnectionsEdit.js
@@ -6,7 +6,6 @@ class TagConnectionsEdit
 	tagsGivesHint = [];
 	idTags = [];
 	popularTags = [];
-	newTag = null;
 
 	add(tsumegoID, tagName)
 	{
@@ -22,12 +21,7 @@ class TagConnectionsEdit
 							newAllTags.push(this.allTags[i]);
 					this.allTags = newAllTags;
 					this.tags.push(tagName);
-					$(".tag-list").html("");
-					$(".add-tag-list").html("");
-					$(".add-tag-list-popular").html("");
 					this.draw();
-					$(".add-tag-list").hide();
-					$(".add-tag-list-popular").hide();
 				}
 			});
 	}
@@ -46,58 +40,30 @@ class TagConnectionsEdit
 							newAllTags.push(this.tags[i]);
 					this.tags = newTags;
 					this.allTags.push(tagName);
-					$(".tag-list").html("");
-					$(".add-tag-list").html("");
-					$(".add-tag-list-popular").html("");
 					this.draw();
-					$(".add-tag-list").hide();
-					$(".add-tag-list-popular").hide();
 				}
 			});
 	}
 
 	draw()
 	{
-		if(this.tags.length>0)
+		$(".tag-list").html("");
+		if(this.tags.length > 0)
 			$(".tag-list").append("Tags: ");
-		let foundNewTag = false;
-		for(let i=0;i<this.tags.length;i++)
+		for(let i = 0;i < this.tags.length; i++)
 		{
-			let isNewTag = '';
-			if(this.tags[i] === this.newTag)
-			{
-				isNewTag = 'is-new-tag';
-				foundNewTag = true;
-			}
-			else if(this.unapprovedTags[i]==0)
-				isNewTag = 'is-new-tag';
-			if (this.tagsGivesHint[i] == 1)
-				isNewTag = 'tag-gives-hint '+isNewTag;
 			let tagLink = 'href="/tag_names/view/' + this.idTags[i]+'"';
 			let tagLinkId = 'id="'+makeIdValidName(this.tags[i])+'"';
-			if(typeof this.idTags[i] === "undefined")
-			{
-				tagLink = '';
-				tagLinkId = '';
-			}
-			$(".tag-list").append('<a '+tagLink+' class="'+isNewTag+'" '+tagLinkId+'>' + this.tags[i] + '</a>');
-			if (i < this.tags.length-1)
-			{
+            let tagLinkClass = this.tagsGivesHint[i] == 1 ? 'tag-gives-hint ' : '';
+			$(".tag-list").append('<a '+tagLink+' class="' + tagLinkClass + '" '+tagLinkId+'>' + this.tags[i] + '</a>');
+			if (i < this.tags.length - 1)
 				if(this.tagsGivesHint[i] == 1)
 					$(".tag-list").append('<p class="tag-gives-hint">, </p>');
-				else if (!this.isLastComma(i, this.tagsGivesHint, this.tags))
-					$(".tag-list").append('<p class="tag-comma">, </p>');
 				else
-					$(".tag-list").append('<p class="tag-gives-hint">, </p>');
-			}
-		}
-		if(foundNewTag)
-		{
-			$(".tag-list").append(" ");
-			$(".tag-list").append('<button id="undo-tags-button">x</button>');
-			$("#undo-tags-button").show();
+					$(".tag-list").append('<p class="tag-comma">, </p>');
 		}
 
+		$(".add-tag-list-popular").html("");
 		$(".add-tag-list-popular").append("Add tag: ");
 		for (let i=0; i < this.popularTags.length; i++)
 		{
@@ -108,6 +74,7 @@ class TagConnectionsEdit
 		}
 		$(".add-tag-list-popular").append(' <a class="add-tag-list-anchor" id="open-more-tags">[more]</a>');
 
+		$(".add-tag-list").html("");
 		$(".add-tag-list").append("Add tag: ");
 		for (let i = 0; i < this.allTags.length; i++)
 		{
@@ -119,15 +86,5 @@ class TagConnectionsEdit
 		$(".add-tag-list").append(' <a class="add-tag-list-anchor" href="/tag_names/add">[Create new tag]</a>');
 		if (problemSolved)
 			$(".tag-gives-hint").css("display", "inline");
-	}
-
-	isLastComma(index, hints, tags)
-	{
-		if(index >= hints.length - 1)
-			return this.newTag == null;
-		for(let i = index + 1; i < hints.length; i++)
-			if(hints[i] == 0)
-				return false;
-		return true;
 	}
 }
