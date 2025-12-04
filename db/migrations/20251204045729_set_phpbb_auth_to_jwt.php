9da3e794fc029091fc7b91332ccfb27ed87bfe74
kovarex <kovarex@gmail.com>
2025-12-05 00:19:55 +0100

    This is just up

--- db/migrations/20251204045729_set_phpbb_auth_to_jwt.php
+++ db/migrations/20251204045729_set_phpbb_auth_to_jwt.php
@@ -12,12 +12,12 @@ final class SetPhpbbAuthToJwt extends AbstractMigration
 	 * JWT auth supports multi-device login (each device gets independent JWT token).
 	 * Old mycustomauth used single database token which overwrote on new login.
 	 */
-	public function change(): void
+	public function up(): void
 	{
 		// Update phpBB config to use JWT authentication
 		$this->execute("
-			UPDATE phpbb_config 
-			SET config_value = 'jwtauth' 
+			UPDATE phpbb_config
+			SET config_value = 'jwtauth'
 			WHERE config_name = 'auth_method'
 		");
 	}
