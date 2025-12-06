d5b75d341d8a12ebeffd2dd65068e179442f9d4a
Filip Buchta <kikaimaru@windowslive.com>
2025-12-06 01:37:53 +0100

    fix: Start button in set navigates to first unsolved puzzle (#95)
    * fix: Start button navigates to first unsolved tsumego
    
    * refactor: Remove redundant comments from self-documenting method calls
    
    * fix: AchievementCondition FK violations
    test: Add AchievementCondition cleanup to ContextPreparator
    
    * revert: Undo AppController set_id change - needs more investigation
    
    * Cleanup
    
    * cs fix
    
    ---------
    
    Co-authored-by: SorcererOnTheRocks <sorcererontherocks@gmail.com>
    Co-authored-by: kovarex <kovarex@gmail.com>

---
+++ tests/TestCase/Controller/SetsViewStartButtonTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use Facebook\WebDriver\WebDriverBy;
+
+/**
+ * Tests the Start button on set view pages.
+ *
+ * This test verifies that clicking the Start button on /sets/view/{setId}
+ * navigates to the FIRST UNSOLVED puzzle in the set (not just the first puzzle).
+ */
+class SetsViewStartButtonTest extends ControllerTestCase
+{
+	public function testStartButtonNavigatesToFirstUnsolvedPuzzle(): void
+	{
+		// Create test context with multiple tsumegos in a test set
+		// First 2 are solved, 3rd is unsolved (target), 4th-5th are also unsolved
+		// Start button should link to #3 (first unsolved in middle of set)
+		$context = new ContextPreparator([
+			'tsumego' => [
+				'sets' => [['name' => 'Test Start Button Set', 'num' => '3']],
+				'status' => 'V' // Not solved - this should be where Start button links to
+			],
+			'other-tsumegos' => [
+				['sets' => [['name' => 'Test Start Button Set', 'num' => '1']], 'status' => 'S'], // Solved
+				['sets' => [['name' => 'Test Start Button Set', 'num' => '2']], 'status' => 'S'], // Solved
+				['sets' => [['name' => 'Test Start Button Set', 'num' => '4']], 'status' => 'V'], // Also unsolved
+				['sets' => [['name' => 'Test Start Button Set', 'num' => '5']], 'status' => 'V'], // Also unsolved
+			]
+		]);
+
+		$browser = Browser::instance();
+
+		// 1. Open the set view page using the created set ID
+		$setId = $context->tsumego['sets'][0]['id'];
+		$browser->get('sets/view/' . $setId);
+
+		// 2. Verify we're on the set view page
+		$this->assertSame(Util::getMyAddress() . '/sets/view/' . $setId, $browser->driver->getCurrentURL());
+
+		// Wait for page to fully load
+		$browser->waitUntilCssSelectorExists('a.new-button.new-buttonx');
+
+		// 3. Find and click the Start button (using CSS selector since linkText didn't work)
+		$startButton = $browser->getCssSelect('a.new-button.new-buttonx')[0];
+		$this->assertTrue($startButton->isDisplayed(), 'Start button should be visible');
+		$startButton->click();
+
+		// 4. Verify we navigated to the FIRST UNSOLVED puzzle (problem #3, not #1)
+		// The URL is /{setConnectionId} (short form)
+		$setConnectionId = $context->tsumego['set-connections'][0]['id'];
+		$expectedUrl = Util::getMyAddress() . '/' . $setConnectionId;
+		$this->assertSame($expectedUrl, $browser->driver->getCurrentURL(), 'Should navigate to first UNSOLVED puzzle (#3 in middle of set), not first puzzle (#1)');
+	}
+}
