b17b757851db609481c2419e7ff813d34308c2ac
kovarex <kovarex@gmail.com>
2025-12-24 10:08:04 +0100

    Deduplicate admin activity rows for tsumegos with more set connections

--- src/Utility/AdminActivityRenderer.php
+++ src/Utility/AdminActivityRenderer.php
@@ -12,29 +12,51 @@ class AdminActivityRenderer
 
 		$this->data = Util::query("
 SELECT
-	admin_activity.created as created,
-	admin_activity_type.id as type,
-	admin_activity_type.name as readable_type,
-	admin_activity.old_value as old_value,
-	admin_activity.new_value as new_value,
-    tsumego.id as tsumego_id,
-    user.id AS user_id,
-    user.name AS user_name,
-    user.picture AS user_picture,
-    user.external_id AS user_external_id,
-    user.rating AS user_rating,
-    set_connection.id AS set_connection_id,
-    set_connection.num AS num,
-    tsumego_status.status AS status,
-    CONCAT(`set`.title, ' ', `set`.title2) AS set_title
+	admin_activity.created AS created,
+	admin_activity_type.id AS type,
+	admin_activity_type.name AS readable_type,
+	admin_activity.old_value AS old_value,
+	admin_activity.new_value AS new_value,
+	tsumego.id AS tsumego_id,
+	user.id AS user_id,
+	user.name AS user_name,
+	user.picture AS user_picture,
+	user.external_id AS user_external_id,
+	user.rating AS user_rating,
+	set_connection.id AS set_connection_id,
+	set_connection.num AS num,
+	tsumego_status.status AS status,
+	CONCAT(`set`.title, ' ', `set`.title2) AS set_title
 FROM
 	admin_activity
-	JOIN admin_activity_type ON admin_activity.type = admin_activity_type.id
-	LEFT JOIN tsumego ON admin_activity.tsumego_id = tsumego.id
-	LEFT JOIN set_connection ON set_connection.tsumego_id = admin_activity.tsumego_id
-	LEFT JOIN user ON admin_activity.user_id = user.id
-	LEFT JOIN `set` ON `set`.id = set_connection.set_id
-	LEFT JOIN tsumego_status ON tsumego_status.user_id = ? AND tsumego_status.tsumego_id = tsumego.id
+	JOIN admin_activity_type
+		ON admin_activity.type = admin_activity_type.id
+	LEFT JOIN tsumego
+		ON admin_activity.tsumego_id = tsumego.id
+
+	/* pick exactly ONE set_connection per tsumego */
+	LEFT JOIN (
+		SELECT *
+		FROM (
+			SELECT
+				set_connection.*,
+				ROW_NUMBER() OVER (
+					PARTITION BY set_connection.tsumego_id
+					ORDER BY set_connection.id
+				) AS rn
+			FROM set_connection
+		) ranked_set_connection
+		WHERE ranked_set_connection.rn = 1
+	) set_connection
+		ON set_connection.tsumego_id = admin_activity.tsumego_id
+
+	LEFT JOIN `set`
+		ON `set`.id = set_connection.set_id
+	LEFT JOIN user
+		ON admin_activity.user_id = user.id
+	LEFT JOIN tsumego_status
+		ON tsumego_status.user_id = ?
+	   AND tsumego_status.tsumego_id = tsumego.id
 ORDER BY admin_activity.id DESC
 LIMIT " . self::$PAGE_SIZE . "
 OFFSET $offset", [Auth::getUserID()]);
