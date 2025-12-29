8a940bd87e79a3975f1204470b006b20f297a232
kovarex <kovarex@gmail.com>
2025-12-29 21:27:10 +0100

    DataTable generalisation start

---
+++ src/Utility/DataTableRenderer.php
@@ -0,0 +1,34 @@
+<?php
+
+class DataTableRenderer
+{
+	public function __construct($urlParams, $name, $caption)
+	{
+		$this->name = $name;
+		$this->caption = $caption;
+		$this->page = isset($urlParams[$this->name]) ? max(1, (int) $urlParams[$this->name]) : 1;
+		$this->pageCount = ceil($this->count / self::$PAGE_SIZE);
+		$this->offset = ($this->page - 1) * self::$PAGE_SIZE;
+	}
+
+	public function render()
+	{
+		echo '<h3>' . $this->caption . ' (' . $this->count . ')</h3>';
+		echo PaginationHelper::render($this->page, $this->pageCount, $this->name);
+		echo '<table border="0" class="statsTable" style="border-collapse:collapse;">';
+		foreach ($this->data as $index => $item)
+			$this->renderItem($index, $item);
+		echo '</table>';
+		echo PaginationHelper::render($this->page, $this->pageCount, $this->name);
+	}
+
+	protected string $name;
+	protected string $caption;
+	protected static $PAGE_SIZE = 100;
+
+	protected array $data = [];
+	protected int $page;
+	protected int $pageCount;
+	protected int $count;
+	protected int $offset;
+}
