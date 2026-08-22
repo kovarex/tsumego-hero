5462761b0c69294007726bf3bf9c86c71e7a6221
SorcererOnTheRocks <sorcererontherocks@gmail.com>
2026-08-22 13:19:45 +0200

    fix: use damage instead of health=0 (PHP falsy)

--- tests/TestCase/Utility/HeroPowersTest.php
+++ tests/TestCase/Utility/HeroPowersTest.php
@@ -331,7 +331,7 @@ class HeroPowersTest extends TestCaseWithAuth
 	{
 		$browser = Browser::instance();
 		$context = new ContextPreparator([
-			'user' => ['level' => HeroPowers::$REJUVENATION_MINIMUM_LEVEL, 'health' => 0],
+				'user' => ['level' => HeroPowers::$REJUVENATION_MINIMUM_LEVEL, 'damage' => Util::getHealthBasedOnLevel(HeroPowers::$REJUVENATION_MINIMUM_LEVEL)],
 			'tsumegos' => [
 				['set_order' => 1]]]);
 		$context->changeUserSoRejuvenationCanBeUsed();
